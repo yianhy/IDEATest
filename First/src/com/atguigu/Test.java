b7bd3d27013b8f0a9473c73f2a2dc74ae4b8e549
yian <yiankonlie@gmail.com>
2023-03-11 17:13:23 +0800

    first

---
+++ First/src/com/atguigu/Test.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: Test
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author 黄洋
+ * @Create 2023/3/11 17:12
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
